4c5256b44684c56b97e3285ce23f1a344eb580aa
AVB9907 <aleidavaldivieso@gmail.com>
2026-04-01 11:36:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -112,8 +112,8 @@ def login(usuario, password):
 
 # ===== SESSION STATE ===== #
 
-if "pagina" not in st.session_state:
-    st.session_state.pagina = "inicio"
+if "pagina" in st.query_params:
+    st.session_state.pagina = st.query_params["pagina"][0]
 
 if "pagina" in st.query_params:
     st.session_state.pagina = st.query_params["pagina"]
